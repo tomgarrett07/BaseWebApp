b2c45b6b308e5b89c273854d4910dd629072c1be
Tom Garrett <tgarrett@hubspot.local>
2021-02-16 14:18:55 -0600

    final version

--- project/js/main.js
+++ project/js/main.js
@@ -1,9 +1,72 @@
-function showPicture(){
-  // use jQuery ($ is shorthand) to find the div on the page and then change the html
-  // 'rounded-circle' is a bootstrap thing! Check out more here: http://getbootstrap.com/css/
-  $("#image").append('<img class="rounded-circle" src="images/high-five.gif"/>');
-  $("p").html("High five! You're building your first web app!");
+$(document).ready(function(){
+  getPosts();
+})
 
-  // jQuery can do a lot of crazy stuff, so make sure to Google around to find out more
-  
+
+function handleSignIn(){
+  var provider = new firebase.auth.GoogleAuthProvider();
+
+  firebase.auth()
+  .signInWithPopup(provider)
+  .then((result) => {
+    /** @type {firebase.auth.OAuthCredential} */
+    var credential = result.credential;
+
+    // This gives you a Google Access Token. You can use it to access the Google API.
+    var token = credential.accessToken;
+    // The signed-in user info.
+    var user = result.user;
+    console.log(user.email);
+  }).catch((error) => {
+    // Handle Errors here.
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    // The email of the user's account used.
+    var email = error.email;
+    // The firebase.auth.AuthCredential type that was used.
+    var credential = error.credential;
+    // ...
+  });
+}
+
+function addMessage(zip,state,city){
+  var postData = {
+      zip: zip,
+      state: state,
+      city: city
+  }
+
+  var database = firebase.database().ref("posts");
+
+  var newPostRef = database.push();
+  newPostRef.set(postData, function(error) {
+  if (error) {
+    // The write failed...
+  } else {
+    // Data saved successfull!
+     window.location.reload()
+  }
+});
+}
+
+function handleMessageFormSubmit(){
+  var zip = $("#zip").val();
+  var state = $("#state").val();
+  var city = $("#city").val();
+  addMessage(zip,state,city);
+
+}
+
+function getPosts(){
+
+  return firebase.database().ref("posts").once('value').then(function(snapshot){
+    var posts = snapshot.val();
+    console.log(posts);
+    
+    for(var postKey in posts) {
+      var post = posts[postKey];
+      console.log(post);
+      $("#post-listing").append(" <div>"+post.city+", "+post.state+"  "+post.zip+"</div> ");
+    }
+  });
 }
